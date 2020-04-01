81264ac369d77e0208df51492bb399e0f4279e23
mingwon <40914543+mingwon@users.noreply.github.com>
2020-04-01 19:36:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,9 @@ async def on_message(message):
         return None #동작하지 않고 무시합니다.
 
     if message.content.startswith('/홀리'):
-        await client.send_message(message.channel, 'https://cdn.discordapp.com/attachments/416968282470350858/694842481686216734/toystorygamza.jpg')
+        embed=discord.Embed(title="sdf", description="sf")
+        embed.add_field(name="undefined", value="undefined", inline=False)
+        await client.send_message(message.channel, embed=embed)
         
     if message.content.startswith('/감자'):
         await client.send_message(message.channel, 'https://cdn.discordapp.com/attachments/416968282470350858/694842481686216734/toystorygamza.jpg')
